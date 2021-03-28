17022129efed36b6780d14fd51e38b8b48823ea0
Aaron Cleveland <toscleveland@gmail.com>
2021-03-28 15:32:46 -0500

    init project | inetdata Done

---
+++ inetdata_start.py
@@ -0,0 +1,14 @@
+# 
+# Example file for retrieving data from the internet
+#
+import urllib.request
+
+def main():
+      webURL = urllib.request.urlopen("http://www.google.com")
+      print("result code: " + str(webURL.getcode()))
+
+      data = webURL.read()
+      print(data)
+
+if __name__ == "__main__":
+  main()
